4e71220a1a8388a21ad47c96f040a22ab49f6ae9
Terrence Brannon <metaperl@gmail.com>
2021-03-29 04:55:09 -0400

    Add license. Refactor reduction.

--- gematria38/mappings.py
+++ gematria38/mappings.py
@@ -5,8 +5,16 @@ from loguru import logger
 
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+def reduce(r):
+    if r <= 9:
+        return r
+    if r <= 18:
+        return r - 9
+    return r - 18
+
 @dataclass
-class Ordinal:
+class Cipher:
+    """A way of converting letters to numbers."""
     
     letters: List[str] = field(default_factory=list)
     numbers: List[int] = field(default_factory=list)
@@ -14,12 +22,22 @@ class Ordinal:
     @property
     def sum(self):
         return sum(self.numbers)
+    
+    def decode(self, s):
+        self.letters = s
+        self.numbers = [self.ord(c) for c in self.letters]
+        return self
+             
 
     def __str__(self):
         return f"""
-    Decoding of '{self.letters}' yields {self.numbers} summing to {self.sum}
+    {self.__class__.__name__} Decoding of '{self.letters}' yields {self.numbers} summing to {self.sum}
     """
-    
+        
+
+@dataclass
+class Ordinal(Cipher):
+
     def ord(self, c):
         if c.isupper():
             return ord(c) - 64
@@ -28,23 +46,14 @@ class Ordinal:
         logger.debug(f"weird data ({c}) passed.")
         return 0
     
-    def decode(self, s):
-        self.letters = s
-        self.numbers = [self.ord(c) for c in self.letters]
-        return self
-             
+
 
 @dataclass
 class Reduced(Ordinal):
     
     def ord(self, c):
         r = super().ord(c)
-        if r <= 9:
-            return r
-        if r <= 18:
-            return r - 9
-        return r - 18
-
+        return reduce(r)
     
 @dataclass
 class ReverseOrdinal(Ordinal):
@@ -59,47 +68,23 @@ class ReverseOrdinal(Ordinal):
     def ord(self, c):
         _ = super().ord(c)
         r = 27 - _
-        logger.debug(f"The character '{c}' {_=}, {r=}")
-        if r <= 9:
-            return r
-        if r <= 18:
-            return r - 9
-        return r - 18
+        return r
 
 
 @dataclass
 class ReverseReduced(ReverseOrdinal):
     def ord(self, c):
         r = super().ord(c)
-        if r <= 9:
-            return r
-        if r <= 18:
-            return r - 9
-        return r - 18
+        return reduce(r)
 
     
 if __name__ == '__main__':
-    test_data = 'azlm'
-    e = Ordinal()
-    for c in test_data:
-        r = e.ord(c)
-        print(f"{c=} -> {r=}.")
-    
+    e1 = Ordinal()
     e2 = Reduced()
-    for c in test_data:
-        r = e2.ord(c)
-        print(f"{c=} -> {r=}.")
-    
     e3 = ReverseOrdinal()
-    for c in test_data:
-        r = e3.ord(c)
-        print(f"{c=} -> {r=}.")
-        
     e4 = ReverseReduced()
-    for c in test_data:
-        r = e4.ord(c)
-        print(f"{c=} -> {r=}.")
-            
-#     print(e2.decode("Morals and Dogma"))
-#  
-#     print(e.decode("Washington, D.C."))
+
+    print(e1.decode(ALPHABET))
+    print(e2.decode(ALPHABET))
+    print(e3.decode(ALPHABET))
+    print(e4.decode(ALPHABET))
